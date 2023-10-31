7ef055a13f2dfef241ccc6239dcd326a65a261c3
danghuuviet <94746432+danghuuviet@users.noreply.github.com>
2023-10-31 14:21:38 +0700

    Fin

---
+++ ex7+ex8/Core/Inc/func.h
@@ -0,0 +1,12 @@
+/*
+ * func.h
+ *
+ *  Created on: Oct 10, 2021
+ *      Author: MSI-
+ */
+
+#ifndef INC_FUNC_H_
+#define INC_FUNC_H_
+
+void update7SEG ( int index, int num );
+#endif /* INC_FUNC_H_ */
